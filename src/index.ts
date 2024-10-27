d34cb24ec4333a29a5a8f49a524dfb194b16814b
haton14 <haton14@haton14.com>
2024-10-28 00:43:33 +0900

    Update tsconfig.json and biome.json, and add OpenTelemetry tracing to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,39 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { type Span, trace } from "@opentelemetry/api";
+import { Resource } from "@opentelemetry/resources";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
+import {
+  SimpleSpanProcessor,
+} from "@opentelemetry/sdk-trace-base";
+import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
+import { ATTR_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
+import { Hono } from "hono";
 
-const app = new Hono()
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+const exporter = new OTLPTraceExporter({
+	url: "http://tomato.ghost-algieba.ts.net:4318/v1/traces",
 })
+const processor = new SimpleSpanProcessor(exporter);
+const tracerProvider = new NodeTracerProvider({
+  resource: new Resource({
+    [ATTR_SERVICE_NAME]: "hono-otlp-example",
+  }),
+});
+tracerProvider.addSpanProcessor(processor);
 
-const port = 3000
-console.log(`Server is running on http://localhost:${port}`)
+tracerProvider.register();
+
+const tracer = trace.getTracer("hono-handler");
+
+const app = new Hono();
+
+app.get("/hello", (c) => {
+  return tracer.startActiveSpan(`${c.req.method}:${c.req.path}`, async (span: Span) => {
+    span.end();
+    return c.text("Hello Hono!\n");
+  });
+});
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port: 3000,
+});
